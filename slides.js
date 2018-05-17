41567c9007c7bb2b8f249db28f5a56677af3388e
Bodil Stokke <bodil@bodil.org>
2018-05-17 13:17:08 +0200

    Make it so.

---
+++ slides.js
@@ -0,0 +1,10 @@
+var Pink = require("pink");
+
+require("pink/css/themes/simon.less");
+require("pink/node_modules/highlight.js/styles/vs.css");
+require("./screen.less");
+
+new Pink("#slides", {
+  "background": require("pink/modules/background"),
+  "image": require("pink/modules/image")
+});
